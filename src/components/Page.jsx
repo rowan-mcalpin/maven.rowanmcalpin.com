6c27f258f03f3e84828c47954d81d8c981fe6935
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-11-14 12:50:13 -0800

    Begin using React

---
+++ src/components/Page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="page">
+            <h1>Rowan McAlpin's Maven Repository</h1>
+            <em><h3>This site is under development.</h3></em>
+        </div>
+    )
+}
