176108b4c53f9df74234876b993760aa7c83cc79
rowan-mcalpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-12-28 23:28:53 -0800

    Release 0.5.0-beta1 -- PEDRO VERSION!

--- src/components/Page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-        <div className="page">
-            <h1>Rowan McAlpin's Maven Repository</h1>
-            <em><h3>This site is under development.</h3></em>
-        </div>
-    )
-}
